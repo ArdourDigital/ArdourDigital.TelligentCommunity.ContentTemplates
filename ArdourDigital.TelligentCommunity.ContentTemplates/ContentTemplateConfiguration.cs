62768ce346755b4bda1fa9177391513230d2b96d
Rhys Godfrey <rhys@ardourdigital.co.uk>
2017-04-23 16:48:35 +0100

    Video 1 - Simple Configurable Plugin

---
+++ ArdourDigital.TelligentCommunity.ContentTemplates/ContentTemplateConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArdourDigital.TelligentCommunity.ContentTemplates
+{
+    public static class ContentTemplateConfiguration
+    {
+        public static string Template { get; set; }
+    }
+}
---
+++ ArdourDigital.TelligentCommunity.ContentTemplates/ContentTemplatesPlugin.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telligent.DynamicConfiguration.Components;
+using Telligent.Evolution.Controls;
+using Telligent.Evolution.Extensibility.Version1;
+
+namespace ArdourDigital.TelligentCommunity.ContentTemplates
+{
+    public class ContentTemplatesPlugin : IPlugin, IConfigurablePlugin
+    {
+        public string Name => "Ardour Digital - Content Templates";
+
+        public string Description => "Allows defined content templates to be inserted using the rich text editor";
+
+        public PropertyGroup[] ConfigurationOptions
+        {
+            get
+            {
+                var group = new PropertyGroup("configuration", "Configuration", 0);
+
+                var property = new Property("template", "Template", PropertyType.Html, 0, string.Empty);
+                property.ControlType = typeof(HtmlEditorStringControl);
+
+                group.Properties.Add(property);
+
+                return new[] { group };
+            }
+        }
+
+        public void Initialize()
+        {
+        }
+
+        public void Update(IPluginConfiguration configuration)
+        {
+            ContentTemplateConfiguration.Template = configuration.GetHtml("template");
+        }
+    }
+}
