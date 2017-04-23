c726a07ad84a830ddca557a0094e48390de21767
Rhys Godfrey <rhys@ardourdigital.co.uk>
2017-04-23 16:53:01 +0100

    Update AssemblyInfo with standard details

--- ArdourDigital.TelligentCommunity.ContentTemplates/Properties/AssemblyInfo.cs
+++ ArdourDigital.TelligentCommunity.ContentTemplates/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ArdourDigital.TelligentCommunity.ContentTemplates")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Ardour Digital - Telligent Community - Content Templates")]
+[assembly: AssemblyDescription("Allows pre-defined content templates to be inserted using the rich text editor")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ArdourDigital.TelligentCommunity.ContentTemplates")]
+[assembly: AssemblyCompany("Ardour Digital Limited")]
+[assembly: AssemblyProduct("Ardour Digital - Telligent Community - Content Templates")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
