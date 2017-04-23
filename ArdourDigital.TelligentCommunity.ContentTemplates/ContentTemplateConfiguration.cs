3a88a4b67b39a03efa79ad4a463996ca674d52ea
Rhys Godfrey <rhys@ardourdigital.co.uk>
2017-04-23 16:50:30 +0100

    Tidy up usings

--- ArdourDigital.TelligentCommunity.ContentTemplates/ContentTemplateConfiguration.cs
+++ ArdourDigital.TelligentCommunity.ContentTemplates/ContentTemplateConfiguration.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ArdourDigital.TelligentCommunity.ContentTemplates
+﻿namespace ArdourDigital.TelligentCommunity.ContentTemplates
 {
     public static class ContentTemplateConfiguration
     {
