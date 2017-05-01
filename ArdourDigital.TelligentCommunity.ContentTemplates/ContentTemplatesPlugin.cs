541d96dc97a4d20d4b2d39752fe97c2db37c7668
Rhys Godfrey <rhys@ardourdigital.co.uk>
2017-05-01 12:26:40 +0100

    Video 3 - Embeddable Content Fragment Plugin

--- ArdourDigital.TelligentCommunity.ContentTemplates/ContentTemplatesPlugin.cs
+++ ArdourDigital.TelligentCommunity.ContentTemplates/ContentTemplatesPlugin.cs
@@ -1,13 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 using Telligent.DynamicConfiguration.Components;
 using Telligent.Evolution.Controls;
+using Telligent.Evolution.Extensibility.EmbeddableContent.Version1;
+using Telligent.Evolution.Extensibility.Storage.Version1;
 using Telligent.Evolution.Extensibility.Version1;
 
 namespace ArdourDigital.TelligentCommunity.ContentTemplates
 {
-    public class ContentTemplatesPlugin : IPlugin, IConfigurablePlugin, ITranslatablePlugin
+    public class ContentTemplatesPlugin : IPlugin, IConfigurablePlugin, ITranslatablePlugin, IEmbeddableContentFragmentType
     {
         private ITranslatablePluginController _translationController;
+        private readonly Guid _typeId = new Guid("7b78ba19-26b5-4985-94ec-40a42d25caea");
 
         public string Name => "Ardour Digital - Content Templates";
 
@@ -36,13 +40,52 @@ namespace ArdourDigital.TelligentCommunity.ContentTemplates
 
                 translations.Set("configuration", "Configuration");
                 translations.Set("template", "Template");
+                translations.Set("name", "Content Template");
+                translations.Set("description", "Insert text from a predefined template");
 
                 return new[] { translations };
             }
         }
 
+        public string ContentFragmentName => _translationController.GetLanguageResourceValue("name");
+
+        public string ContentFragmentDescription => _translationController.GetLanguageResourceValue("description");
+
+        public Guid EmbeddedContentFragmentTypeId => _typeId;
+
+        public PropertyGroup[] EmbedConfiguration
+        {
+            get
+            {
+                var group = new PropertyGroup("configuration", _translationController.GetLanguageResourceValue("configuration"), 0);
+
+                var value = new Property("value", _translationController.GetLanguageResourceValue("template"), PropertyType.Html, 0, ContentTemplateConfiguration.Template);
+                value.ControlType = typeof(HtmlEditorStringControl);
+
+                group.Properties.Add(value);
+
+                return new[] { group };
+            }
+        }
+
+        public string PreviewImageUrl => CentralizedFileStorage.GetGenericDownloadUrl("system", "images", "embedcode.png");
+
+        public void AddUpdateContentFragments(Guid contentId, Guid contentTypeId, IEnumerable<IEmbeddableContentFragment> embeddedFragments)
+        {
+        }
+
+        public bool CanEmbed(Guid contentTypeId, int userId)
+        {
+            return true;
+        }
+
         public void Initialize()
         {
+        }
+
+        public string Render(IEmbeddableContentFragment embeddedFragment, string target)
+        {
+            return embeddedFragment.GetHtml("value");
         }
 
         public void SetController(ITranslatablePluginController controller)
@@ -53,6 +96,11 @@ namespace ArdourDigital.TelligentCommunity.ContentTemplates
         public void Update(IPluginConfiguration configuration)
         {
             ContentTemplateConfiguration.Template = configuration.GetHtml("template");
+        }
+
+        public EmbeddableContentFragmentValidationState Validate(IEmbeddableContentFragment embeddedFragment)
+        {
+            return new EmbeddableContentFragmentValidationState(true);
         }
     }
 }
