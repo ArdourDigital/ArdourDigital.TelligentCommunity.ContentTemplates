c9b36d4b3fc7a733d96a8dba4505390048c681c7
Rhys Godfrey <rhys@ardourdigital.co.uk>
2017-05-01 12:13:36 +0100

    Video 2 - Add Translation

--- ArdourDigital.TelligentCommunity.ContentTemplates/ContentTemplatesPlugin.cs
+++ ArdourDigital.TelligentCommunity.ContentTemplates/ContentTemplatesPlugin.cs
@@ -1,11 +1,14 @@
-﻿using Telligent.DynamicConfiguration.Components;
+﻿using System;
+using Telligent.DynamicConfiguration.Components;
 using Telligent.Evolution.Controls;
 using Telligent.Evolution.Extensibility.Version1;
 
 namespace ArdourDigital.TelligentCommunity.ContentTemplates
 {
-    public class ContentTemplatesPlugin : IPlugin, IConfigurablePlugin
+    public class ContentTemplatesPlugin : IPlugin, IConfigurablePlugin, ITranslatablePlugin
     {
+        private ITranslatablePluginController _translationController;
+
         public string Name => "Ardour Digital - Content Templates";
 
         public string Description => "Allows defined content templates to be inserted using the rich text editor";
@@ -14,9 +17,9 @@ namespace ArdourDigital.TelligentCommunity.ContentTemplates
         {
             get
             {
-                var group = new PropertyGroup("configuration", "Configuration", 0);
+                var group = new PropertyGroup("configuration", _translationController.GetLanguageResourceValue("configuration"), 0);
 
-                var property = new Property("template", "Template", PropertyType.Html, 0, string.Empty);
+                var property = new Property("template", _translationController.GetLanguageResourceValue("template"), PropertyType.Html, 0, string.Empty);
                 property.ControlType = typeof(HtmlEditorStringControl);
 
                 group.Properties.Add(property);
@@ -25,8 +28,26 @@ namespace ArdourDigital.TelligentCommunity.ContentTemplates
             }
         }
 
+        public Translation[] DefaultTranslations
+        {
+            get
+            {
+                var translations = new Translation("en-US");
+
+                translations.Set("configuration", "Configuration");
+                translations.Set("template", "Template");
+
+                return new[] { translations };
+            }
+        }
+
         public void Initialize()
         {
+        }
+
+        public void SetController(ITranslatablePluginController controller)
+        {
+            _translationController = controller;
         }
 
         public void Update(IPluginConfiguration configuration)
